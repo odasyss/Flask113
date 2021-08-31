b7c1352e088fb6cee47348fd2a2e19b4f168e514
Odasys <odasyss@gmail.com>
2021-08-30 18:31:48 -0700

    last Update

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.bool('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party,
     'crispy_forms',
-    'whitenose.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     # local,
     'accounts',
     'pages',
